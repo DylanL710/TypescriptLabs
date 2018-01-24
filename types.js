5a4d6a5279507dc76bcdf73268cd58abcff54880
DylanL710 <G00347849@gmit.ie>
2018-01-24 15:40:39 +0000

    howya types der now

---
+++ types.js
@@ -0,0 +1,10 @@
+var isDone = false;
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var color = "blue";
+color = 'red';
+var fullName = "Bob Bobbington";
+var age = 37;
+var list = [1, 2, 3];
---
+++ types.ts
@@ -0,0 +1,14 @@
+let isDone: boolean = false;
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+let color: string = "blue";
+color = 'red';
+
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+
+let list: number[] = [1, 2, 3];
